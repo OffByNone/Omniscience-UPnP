54b60723bfdcba9b57d7ad1ef52f47f10570e6fb
OffByNone <jeremy.mcneil@gmail.com>
2015-08-22 15:54:41 -0500

    removed bin folder, added npm build scripts, stopped hiding node_modules, updated dependencies

--- bin/StringUtilities.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports.format = function format(stringToFormat) {
-    //a string.format like function
-    var args = Array.prototype.slice.call(arguments, 1);
-    return stringToFormat.replace(/{(\d+)}/g, function (match, number) {
-        return typeof args[number] !== "undefined" ? args[number] : match;
-    });
-};
